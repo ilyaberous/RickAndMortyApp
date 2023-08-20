e172c957d42afd95961291b3694ecf7179ba0093
Ilya-Tatsenko <ilya.tatsenko@yandex.ru>
2023-08-20 21:42:15 +0300

    Initial Commit

---
+++ RickAndMortyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickMortyApp
+//
+//  Created by Ilya on 20.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
